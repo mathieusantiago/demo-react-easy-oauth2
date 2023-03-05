c271d36d5e315d7fa92dffea9d2ed7ba4f69ca3d
mathieusantiago <mathieusantiago21@gmail.com>
2023-03-05 05:29:30 +0100

    adding logo animation composent

---
+++ src/Form.js
@@ -0,0 +1,216 @@
+import { FormConnect } from "react-easy-oauth2";
+import { Col, Container, Form, Image, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Brandlogo from "./assets/react-easy-oauth2.png";
+import "./App.css";
+
+function App() {
+  const [bgColor, setBgColor] = useState("white");
+  const [textColor, setTextColor] = useState("black");
+  const [h1Color, setH1Color] = useState("#34698c");
+  const [btnBgColor, setBtnBgColor] = useState("#34698c");
+  const [linkColor, setLinkColor] = useState("#34698c");
+
+  const styles = {
+    bgComponent: {
+      backgroundColor: bgColor,
+    },
+    textStyle: {
+      color: textColor,
+    },
+    h1Style: {
+      color: h1Color,
+    },
+    btnStyle: {
+      backgroundColor: btnBgColor,
+    },
+    linkStyle: {
+      color: linkColor,
+    },
+  };
+
+  const catchPayload = (payload) => {
+    console.log("catchPayload", payload);
+  };
+  const [signUp, setSignUp] = useState("signIn");
+  const [lang, setLang] = useState("En");
+  const [provider, setProvider] = useState([]);
+  const [logo, setLogo] = useState(
+    "https://cdn-icons-png.flaticon.com/512/3387/3387987.png"
+  );
+
+  const [field, setField] = useState([
+    "fname",
+    "lname",
+    "email",
+    "passwd",
+    "add1",
+    "add2",
+    "phone",
+    "city",
+    "zip",
+  ]);
+
+  if (field[0] === "") {
+    setField([
+      "fname",
+      "lname",
+      "email",
+      "passwd",
+      "add1",
+      "add2",
+      "phone",
+      "city",
+      "zip",
+    ]);
+  }
+
+  const split = (string) => {
+    return string.split(",");
+  };
+
+  const FormConnectComponent = () => {
+    return (
+      <FormConnect
+        url={"https://wpxvwfcdmgcsvczglefh.supabase.co"}
+        apiKey={
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndweHZ3ZmNkbWdjc3ZjemdsZWZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzcyODQ5NTIsImV4cCI6MTk5Mjg2MDk1Mn0.n84d9xUJmuxdigF8aFstFW4DRVw1dBLDx4Lb4JorMts"
+        }
+        catchPayload={catchPayload}
+        logo={logo}
+        useDefault={signUp}
+        field={field}
+        provider={provider}
+        lang={lang}
+        theme={styles}
+      />
+    );
+  };
+  return (
+    <div>
+      <Container>
+        <Row className="align-items-center">
+          <Col md={1}>
+            <Image src={Brandlogo} className="imageBrand" fluid={true} />
+          </Col>
+          <Col md={11} className='text-light'>
+            <h1>React Easy oAuth 2</h1>
+          </Col>
+        </Row>
+        <Row className="mt-5">
+          <Col>
+            <FormConnectComponent />
+          </Col>
+          <Col className="formContainer">
+            <Row>
+              <Form.Group as={Col} md="6" controlId="useDefault">
+                <Form.Label>useDefault</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="Default SignIn"
+                  onChange={(e) => setSignUp(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group as={Col} md="6" controlId="Lang">
+                <Form.Label>Lang</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="Default En"
+                  onChange={(e) => setLang(e.target.value)}
+                />
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+              <Form.Group controlId="logo">
+                <Form.Label>Source logo</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="https://cdn-icons-png.flaticon.com/512/3387/3387987.png"
+                  defaultValue="https://cdn-icons-png.flaticon.com/512/3387/3387987.png"
+                  onChange={(e) => setLogo(e.target.value)}
+                />
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+              <Form.Group controlId="Provider">
+                <Form.Label>Provider liste</Form.Label>
+                <p>please space provider with ","</p>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="Default All"
+                  onChange={(e) => setProvider(split(e.target.value))}
+                />
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+              <Form.Group controlId="Field">
+                <Form.Label>Field liste</Form.Label>
+                <p>please space provider with ","</p>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="Default All"
+                  onChange={(e) => setField(split(e.target.value))}
+                />
+              </Form.Group>
+            </Row>
+
+            <Row>
+              <Form.Group as={Col} md="6" controlId="Backgroundcolor">
+                <Form.Label>Background color:</Form.Label>
+                <Form.Control
+                  type="text"
+                  defaultValue={bgColor}
+                  onChange={(e) => setBgColor(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group as={Col} md="6" controlId="Textcolor">
+                <Form.Label>Text color:</Form.Label>
+                <Form.Control
+                  type="text"
+                  defaultValue={textColor}
+                  onChange={(e) => setTextColor(e.target.value)}
+                />
+              </Form.Group>
+            </Row>
+            <Row>
+              <Form.Group as={Col} md="6" controlId="H1color">
+                <Form.Label>H1 color:</Form.Label>
+                <Form.Control
+                  type="text"
+                  defaultValue={h1Color}
+                  onChange={(e) => setH1Color(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group as={Col} md="6" controlId="Buttonbackgroundcolor">
+                <Form.Label>Button background color:</Form.Label>
+                <Form.Control
+                  type="text"
+                  defaultValue={btnBgColor}
+                  onChange={(e) => setBtnBgColor(e.target.value)}
+                />
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+              <Form.Group controlId="Linkcolor">
+                <Form.Label>Link color:</Form.Label>
+                <Form.Control
+                  type="text"
+                  defaultValue={linkColor}
+                  onChange={(e) => setLinkColor(e.target.value)}
+                />
+              </Form.Group>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Scene.js
@@ -0,0 +1,166 @@
+import React, { useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+import Matter, { Bodies, Composites, Engine, Mouse, MouseConstraint, Render, Runner, World } from "matter-js";
+import { sample } from "lodash";
+import github from "./assets/github-icon.svg";
+import google from "./assets/google-icon.svg";
+import facebook from "./assets/facebook-icon.svg";
+import twitter from "./assets/twitter-icon.svg";
+import linkedin from "./assets/linkedin-icon.svg";
+import discord from "./assets/discord-icon.svg";
+import spotify from "./assets/spotify-icon.svg";
+import twitch from "./assets/twitch-icon.svg";
+import zoom from "./assets/zoom-icon.svg";
+import slack from "./assets/slack-icon.svg";
+import apple from "./assets/apple-icon.svg";
+import microsoft from "./assets/microsoft-icon.svg";
+import gitlab from "./assets/gitlab-icon.svg";
+import bitbucket from "./assets/bitbucket-icon.svg";
+import notion from "./assets/notion-icon.svg";
+import workos from "./assets/workos-icon.svg";
+import keycloak from "./assets/keycloak-icon.svg";
+
+const Scene = () => {
+  const sceneRef = useRef(null);
+
+  useEffect(() => {
+  
+    const engine = Engine.create({
+      positionIterations: 20,
+    });
+
+    const render = Render.create({
+      element: sceneRef.current,
+      engine: engine,
+
+      options: {
+        width: 1950,
+        height: 1250,
+        fillStyle: "white",
+        wireframes: false,
+      },
+    });
+
+    const images = [
+      github,
+      google,
+      facebook,
+      twitter,
+      linkedin,
+      discord,
+      spotify,
+      twitch,
+      zoom,
+      slack,
+      apple,
+      microsoft,
+      gitlab,
+      bitbucket,
+      notion,
+      workos,
+      keycloak,
+    ];
+
+    const stack = Composites.stack(50, 20, 10, 5, 5, 5, function (x, y) {
+      switch (Math.round(Math.random(0, 1))) {
+        case 0:
+          if (Math.random() < 0.8) {
+            return Bodies.rectangle(
+              Math.random() * window.innerWidth,
+              Math.random() * window.innerHeight,
+              80,
+              80,
+              {
+                restitution: 0.5,
+                render: {
+                  sprite: {
+                    texture: sample(images),
+                    xScale: 0.2,
+                    yScale: 0.2,
+                  },
+                },
+              }
+            );
+          } else {
+            return Bodies.rectangle(
+              Math.random() * window.innerWidth,
+              Math.random() * window.innerHeight,
+              80,
+              80,
+              {
+                restitution: 0.5,
+                render: {
+                  sprite: {
+                    texture: sample(images),
+                    xScale: 0.2,
+                    yScale: 0.2,
+                  },
+                },
+              }
+            );
+          }
+        case 1:
+          return Bodies.polygon(x, y, 100, Math.random(50, 10));
+        default:
+          break;
+      }
+    });
+
+    World.add(engine.world, [
+      Bodies.rectangle(0, 940, render.options.width * 2, 20, {
+        isStatic: true,
+        render: { fillStyle: "#232323" },
+      }),
+    ]);
+
+    World.add(engine.world, stack);
+
+    const mouse = Mouse.create(render.canvas);
+    const mouseConstraint = MouseConstraint.create(engine, {
+      mouse: mouse,
+      constraint: {
+        stiffness: 0.2,
+        render: {
+          visible: false,
+        },
+      },
+    });
+    World.add(engine.world, mouseConstraint);
+
+    Matter.Events.on(mouseConstraint, "mousedown", function (event) {
+      World.add(
+        engine.world,
+        Bodies.rectangle(
+          Math.random() * window.innerWidth,
+          Math.random() * window.innerHeight,
+          80,
+          80,
+          {
+            restitution: 0.5,
+            render: {
+              sprite: {
+                texture: sample(images),
+                xScale: 0.1,
+                yScale: 0.1,
+              },
+            },
+          }
+        )
+      );
+    });
+
+    Runner.run(engine);
+
+    Render.run(render);
+
+    return () => {
+      Render.stop(render);
+      World.clear(engine.world);
+      Engine.clear(engine);
+    };
+  }, []);
+
+  return <div ref={sceneRef} />;
+};
+
+export default Scene;
